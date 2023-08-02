23bc3ac175bbc9c875ef12101746d89015922719
sunny park <sunnyparkk@gmail.com>
2023-08-01 22:47:08 -0400

    export getDiffInDays utility

--- src/utils/sort.ts
+++ src/utils/sort.ts
@@ -1,4 +1,4 @@
-const getDiffInDays = (date1: Date, date2: Date) => {
+export const getDiffInDays = (date1: Date, date2: Date) => {
 	const oneDay = 24 * 60 * 60 * 1000;
 	return Math.round(Math.abs((date1.getTime() - date2.getTime()) / oneDay));
 };
