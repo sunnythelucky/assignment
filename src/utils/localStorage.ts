9eace0be6f234cd807caf714791522978134527d
sunny park <sunnyparkk@gmail.com>
2023-08-03 00:11:49 -0400

    Delete localStorage utility

--- src/utils/localStorage.ts
+++
@@ -1,8 +0,0 @@
-export const saveState = (state: any) => {
-	localStorage.setItem("tableState", JSON.stringify(state));
-};
-
-export const loadState = () => {
-	const savedState = localStorage.getItem("tableState");
-	return savedState ? JSON.parse(savedState) : null;
-};
